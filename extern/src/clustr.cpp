5c376c1b2ccfa25e42150cd2eeccb01f13cb1d90
Mengqing Wu <mengqing.wu@desy.de>
2019-03-11 16:55:02 +0100

    Forgot clustr.cpp

---
+++ extern/src/clustr.cpp
@@ -0,0 +1,20 @@
+//        _
+//       | | _
+//     _ | || |
+//    | || || |
+//    | || || |
+
+
+#include "clustr.h"
+
+using namespace std;
+
+clustr::clustr()
+{
+	CoG = 0;
+	Charge = 0;
+}
+
+clustr::~clustr() { }
+
+
